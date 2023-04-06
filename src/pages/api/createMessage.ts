b7673625d43de88220cf31d051b6cde35c5b4dea
Vadim Dagman <vadim@toptal.com>
2023-04-06 14:18:24 -0700

    added API key

--- src/pages/api/createMessage.ts
+++ src/pages/api/createMessage.ts
@@ -23,6 +23,6 @@ export default async function createMessage(req: NextApiRequest, res: NextApiRes
     const data = await response.json()
     res.status(200).json({ data })
   } catch (error) {
-    res.status(500).json({ error: error.message })
+    res.status(500).json({ error: error })
   }
 }
